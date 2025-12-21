8052a43be1230d268dffabd748bb0a782981f5a2
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-12-22 01:39:29 +0700

    feat: Add Axios API client with basic authentication interceptor.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Change this to your Railway URL when deployed
-const API_URL = 'http://127.0.0.1:8000/api/';
+const API_URL = 'web-production-1366c.up.railway.app/api/';
 
 const api = axios.create({
     baseURL: API_URL,
